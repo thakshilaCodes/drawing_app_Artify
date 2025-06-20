f76a600e89581e009d5654a4817c9b86fcd64ec9
Thakshila Fonseka <thakshilafonseka2002@gmail.com>
2025-06-20 10:32:52 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/drawing_app_artify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drawing_app_artify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: false
+      ),
+      home:Scaffold(body: Center(child: Text("Welcome to flutter Guys"),),)
+    );
+  }
+}
+
