b4c414715ff2557f08f1a1a75ef0c8d8bbd05d1c
Thakshila Fonseka <thakshilafonseka2002@gmail.com>
2025-06-20 20:26:09 +0530

    Committing Changes

---
+++ lib/features/draw/presentation/drawscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawScreen extends StatelessWidget {
+  const DrawScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
