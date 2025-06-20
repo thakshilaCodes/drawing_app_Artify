29825e42bd91cd25fb5f0e7117c7539c8937c1e8
Thakshila Fonseka <thakshilafonseka2002@gmail.com>
2025-06-21 02:03:09 +0530

    Committing Changes

---
+++ lib/features/draw/models/stroke.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Stroke {
+  final List<Offset> points;
+  final Color color;
+  final double brushSize;
+
+  Stroke({required this.points, required this.color, required this.brushSize});
+}
--- lib/features/draw/presentation/drawscreen.dart
+++ lib/features/draw/presentation/drawscreen.dart
@@ -1,10 +1,37 @@
+import 'dart:core';
+
+import 'package:drawing_app_artify/features/draw/models/stroke.dart';
 import 'package:flutter/material.dart';
 
-class DrawScreen extends StatelessWidget {
+class DrawScreen extends StatefulWidget {
   const DrawScreen({super.key});
 
   @override
+  State<DrawScreen> createState() => _DrawScreenState();
+}
+
+class _DrawScreenState extends State<DrawScreen> {
+  List<Stroke> _strokes = [];
+  List<Stroke> _redoStrokes = [];
+  List<Offset> _currentPoints = [];
+  Color _selectedColor = Colors.black;
+  double _brushSize = 4.0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Draw Your Dreams'), centerTitle: true),
+      body: Column(
+        children: [
+          GestureDetector(
+            onPanStart: (details) {
+              setState(() {
+                _currentPoints.add(details.localPosition);
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
